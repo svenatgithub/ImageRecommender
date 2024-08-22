eb6d8d17a922875e1e21cebf5db08a4db338f7d0
Anna Popova <anna.popova@study.hs-duesseldorf.de>
2024-08-22 20:46:41 +0200

    Update config.py
    Found the issue with how images and their paths are being loaded to embeddings.py and fixed this.

--- Code/config.py
+++ Code/config.py
@@ -1,9 +1,8 @@
 import os
 
 # Base paths
-BASE_PATH = r"D:\raw_data\Code"
-PATH_TO_SSD = r"D:\raw_data\data"
-
+BASE_PATH = r"D:\Code"
+PATH_TO_SSD = r"D:\raw_data\small_data\Landscapes"
 
 # Database path
 DB_PATH = os.path.join(BASE_PATH, 'collected_data', 'metadata.db')
@@ -15,7 +14,11 @@ COLLECTED_DATA_PATH = os.path.join(BASE_PATH, 'collected_data')
 RGB_PATH = os.path.join(COLLECTED_DATA_PATH, 'rgb_histograms.csv')
 HSV_PATH = os.path.join(COLLECTED_DATA_PATH, 'hsv_histograms.csv')
 
-PICKLE_PATH = os.path.join(BASE_PATH, 'pickles', 'checkpoint.pkl')
+PICKLE_PATH = os.path.join(BASE_PATH, 'pickles')
+CHECKPOINT_PATH = os.path.join(PICKLE_PATH, 'checkpoints')
+
+EMBEDDINGS_FILE = "embeddings.pkl"
+FINAL_EMBEDDINGS_PATH = os.path.join(PICKLE_PATH, EMBEDDINGS_FILE)
 
 # Ensure the pickle directory exists
 os.makedirs(PICKLE_PATH, exist_ok=True)
@@ -23,14 +26,13 @@ os.makedirs(PICKLE_PATH, exist_ok=True)
 # Processing parameters
 COMPRESS_QUALITY = 75
 CHUNK_SIZE = 100
-TOTAL_IMAGES = 444.891 # Total number of images to process
+TOTAL_IMAGES = 3547 # Total number of images to process
 
 # Image processing parameters
-MAX_IMAGE_SIZE = (512, 512)  
-TARGET_IMAGE_SIZE = (224, 224)  # Resizing the images For EfficientNet b0
+MAX_IMAGE_SIZE = (512, 512)  # Define the maximum image size
+TARGET_IMAGE_SIZE = (224, 224)  # For resizing images
 
 ALLOWED_IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.svg')
 
 # Checkpoint parameters
-CHECKPOINT_INTERVAL = 1000  # Interval at which checkpoints are saved
-CHECKPOINT_PATH = os.path.join(PICKLE_PATH, 'checkpoint.pkl')  # Path to save checkpoint data
+CHECKPOINT_INTERVAL = 100  # Interval at which checkpoints are saved
